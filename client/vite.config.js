9e3ae4c9fd7f18a8c34bebd3417ec9002ff2b776
Vaibhav Gautam <vaibhavgautam801@gmail.com>
2026-05-16 14:04:39 +0530

    Linked railway

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     port: 5173,
     proxy: {
-      "/api": "https://vertexflow.onrender.com"
+      "/api": "vertexflow-production-a336.up.railway.app"
     }
   }
 });
