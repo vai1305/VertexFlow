45eeb3f444a1fd92b231b7cd8ba5d88727abe0f3
Vaibhav Gautam <vaibhavgautam801@gmail.com>
2026-05-16 14:13:30 +0530

    Vite configured

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,10 +3,22 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+
   server: {
     port: 5173,
+
     proxy: {
-      "/api": "vertexflow-production.up.railway.app"
+      "/api": {
+        target: "https://vertexflow-production-a336.up.railway.app",
+        changeOrigin: true,
+        secure: true
+      }
     }
+  },
+
+  preview: {
+    allowedHosts: [
+      "vertexflow-production.up.railway.app"
+    ]
   }
 });
